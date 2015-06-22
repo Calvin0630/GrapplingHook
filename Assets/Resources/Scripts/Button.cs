5b79ef5900794ee32eec37a4b4d680c4b409a312
Calvin0630 <cg7057705@gmail.com>
2015-06-22 17:55:55 -0400

    UI and shit

---
+++ Assets/Resources/Scripts/Button.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public void LoadMultiplayer() {
+		Application.LoadLevel ("FirstLevel");
+	}
+}
--- Assets/Resources/Scripts/GameOverDetector.cs
+++ Assets/Resources/Scripts/GameOverDetector.cs
@@ -13,6 +13,11 @@ public class GameOverDetector : MonoBehaviour {
 	
 	}
 	void OnCollisionEnter(Collision col) { 
-
+		if (col.gameObject.tag == "Player1") {
+			Application.LoadLevel("P2Wins");
+		} 
+		else if (col.gameObject.tag == "Player2") {
+			Application.LoadLevel("P1Wins");
+		}
 	}
 }

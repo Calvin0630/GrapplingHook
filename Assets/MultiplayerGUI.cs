a4e0882613bb13fff28fdf9a0ebfaf76f994d7f4
Calvin0630 <cg7057705@gmail.com>
2015-06-22 23:49:48 -0400

    I dont even know

--- Assets/MultiplayerGUI.cs
+++ Assets/MultiplayerGUI.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnintyEngine.UI;
 using System.Collections;
 
 public class MultiplayerGUI : MonoBehaviour {
 	public GameObject player1;
 	public GameObject player2;
 	public string score = "Score: 0";
+	public GUIText currentScore;
 	// Use this for initialization
 	void Start () {
 	

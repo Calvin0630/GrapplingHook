345a8f6b1b656b7d5968aff1080c5189b4fd3685
Calvin0630 <cg7057705@gmail.com>
2015-07-13 23:22:28 -0400

    Started working on obstavle spawner

--- Assets/Resources/Scripts/ObstacleSpawner.cs
+++ Assets/Resources/Scripts/ObstacleSpawner.cs
@@ -2,69 +2,27 @@
 using System.Collections;
 
 public class ObstacleSpawner : MonoBehaviour {
-    public GameObject wall;
-	public bool isActive;
-    public float levelUpDelay;
-    public float initialSpawnDelay;
-	public float minSpawnDelay;
-	float delay;
-    public float deltaSpawnDelayPerLevel;
-    public float xSpeed;
-    public float yScale;
-    public GameObject[] ground;
-	Vector3 TopRightEdgeOfCamera;
-	float widthOfValley = 13.28f;
-	float centerOfValleyX = -1.25f;
-	// Use this for initialization
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject[] obstacles;
+	GameObject farthestPlayer;
+	// is in [-1, 1]
+	public float movingPointX;
+	float cameraWidth;
+	float worldMovePointX;
+
+    // Use this for initialization
 	void Start () {
-		TopRightEdgeOfCamera = -Camera.main.ScreenToWorldPoint (new Vector3 (0, 0, 0));
-		delay = initialSpawnDelay;
-		if (isActive) {
-            StartGroundMoving();
-			StartCoroutine (SpawnCube ());
-			StartCoroutine (LevelUp ());
-		}
+		cameraWidth = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).x;
+		worldMovePointX = movingPointX * cameraWidth;
 	}
 	
 	// Update is called once per frame
     void Update() {
-        if (Time.time < levelUpDelay){
-             
-        }
-	}
-    
-	IEnumerator LevelUp() {
-		while (true) {
-			yield return new WaitForSeconds(levelUpDelay);
-			if (delay > minSpawnDelay) {
-				delay -= deltaSpawnDelayPerLevel;
-			}
+		if (player1.transform.position.x > worldMovePointX || player2.transform.position.x > worldMovePointX) {
+			
 		}
+        
 	}
-
-    IEnumerator SpawnCube() {
-        while (true) {
-			CreateCube ();
-            yield return new WaitForSeconds(delay);
-        }
-    }
     
-    public void StartGroundMoving() {
-        for (int i = 0; i < ground.Length; i++) {
-            ground[i].GetComponent<Rigidbody>().velocity = new Vector3(-xSpeed, 0, 0);
-        }
-    }
-
-    public void CreateCube() {
-        float width = Random.Range(4, 8);
-		float leftOrRight = Random.Range (1, 10) % 2;
-		float xPos = widthOfValley/2 -width/2;
-		if (leftOrRight == 1)
-			xPos *= -1;
-		xPos += centerOfValleyX;
-        GameObject clone = (GameObject) Instantiate(wall, new Vector3(xPos, 2*TopRightEdgeOfCamera.y, 0) , Quaternion.identity);
-		clone.transform.localScale = new Vector3 (width, yScale, 1);
-		clone.GetComponent<Rigidbody> ().velocity = new Vector3 (0, -xSpeed, 0);
-
-    }
 }

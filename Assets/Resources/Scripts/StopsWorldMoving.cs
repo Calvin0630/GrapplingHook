72c5a6b69efdc49eea6f3c17be7419f13d0bec93
Calvin0630 <cg7057705@gmail.com>
2016-01-22 15:45:04 -0500

    stuff

---
+++ Assets/Resources/Scripts/StopsWorldMoving.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+//this scirpt is only here because FUCK UNITY. It should be replaced with OnLevelWasLoaded in StartGame.cs when unity gets their head out of their ass.
+//FUCK UNITY
+//FUCK UNITY
+//FUCK UNITY
+//FUCK UNITY
+//FUCK UNITY
+//FUCK UNITY
+//FUCK UNITY
+public class StopsWorldMoving : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ObstacleSpawner.worldMovingIsEnabled = false;
+	}
+	
+    void OnDestroy() {
+        ObstacleSpawner.worldMovingIsEnabled = true;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
